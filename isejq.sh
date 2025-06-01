a4bd1bb4521b7dedc6f7421470f63394e5ceb249
yamagen <hilofumi.yamamoto@gmail.com>
2025-06-01 19:08:15 +0900

    dan 77 all natural gloss added.

--- isejq.sh
+++ isejq.sh
@@ -12,9 +12,17 @@ jq '[.[] | select(.paragraph != null) |
       + (if .kana != null then {kana: .kana} else {} end)
       + (if ."honkoku" != null then {"honkoku": ."honkoku"} else {} end)
       + (if ."translation-ja" != null then {"translation-ja": ."translation-ja"} else {} end)
-      + (if ."translation-ja-natural" != null then {"translation-ja-natural": ."translation-ja-natural"} else {} end)
       + (if ."translation-en" != null then {"translation-en": ."translation-en"} else {} end)
+      + (if ."phrase-gloss" != null then {"phrase-gloss": ."phrase-gloss"} else {} end)
+      + (if ."gloss-notes-ja" != null then {"gloss-notes-ja": ."gloss-notes-ja"} else {} end)
+      + (if ."gloss-notes-en" != null then {"gloss-notes-en": ."gloss-notes-en"} else {} end)
+      + (if ."glossary-abbreviations" != null then {"glossary-abbreviations": ."glossary-abbreviations"} else {} end)
+      + (if ."translation-ja-natural" != null then {"translation-ja-natural": ."translation-ja-natural"} else {} end)
       + (if ."translation-en-natural" != null then {"translation-en-natural": ."translation-en-natural"} else {} end)
+      + (if ."notes-ja" != null then {"notes-ja": ."notes-ja"} else {} end)
+      + (if ."notes-en" != null then {"notes-en": ."notes-en"} else {} end)
+#      + (if ."commentary-ja" != null then {"commentary-ja": ."commentary-ja"} else {} end)
+#      + (if ."commentary-en" != null then {"commentary-en": ."commentary-en"} else {} end)
     ]
   }
 ]' $1
